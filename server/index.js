4025eea41ed3f4cf53a542ddb82f0e0f83693e25
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-19 15:22:54 +0700

    Connecting to the mongodb database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://haitn:911225@learnit.326ui.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    console.log("MongoDB connected!!!");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(200).send("See you Space Cowboy");
