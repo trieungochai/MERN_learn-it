1c66fc40bc992b9d21b767f4f9919ed2324038a2
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-20 14:06:50 +0700

    Adding /api/auth/login router

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -7,7 +7,7 @@ const JWT = require("jsonwebtoken");
 const authRouter = express.Router();
 
 // @route POST api/auth/register
-// @desc Register user
+// @desc User Registration
 // @access Public
 authRouter.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -16,7 +16,7 @@ authRouter.post("/register", async (req, res) => {
   if (!username || !password)
     return res
       .status(400)
-      .json({ success: false, message: "Missing username and/or password" });
+      .json({ success: false, message: "Missing Username and/or Password" });
 
   try {
     // Check for existing user
@@ -24,7 +24,7 @@ authRouter.post("/register", async (req, res) => {
     if (user)
       return res.status(400).json({
         success: false,
-        message: "This username is already taken",
+        message: "This Username is already taken",
       });
 
     // All good
@@ -40,6 +40,54 @@ authRouter.post("/register", async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Your account has been successfully created",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+// @route POST api/auth/login
+// @desc User Login
+// @access Public
+authRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Validation
+  if (!username || !password)
+    return res.status(400).json({
+      success: false,
+      message: "Missing Username and/or Password",
+    });
+
+  try {
+    // Check for existing user
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.status(400).json({
+        success: false,
+        message: "The Username or Password is Incorrect",
+      });
+
+    // Username has found
+    const validPassword = await argon2.verify(user.password, password);
+    if (!validPassword)
+      return res.status(400).json({
+        success: false,
+        message: "The Username or Password is Incorrect",
+      });
+
+    // All good
+    const accessToken = JWT.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res.status(200).json({
+      success: true,
+      message: "You are successfully signed in",
       accessToken,
     });
   } catch (error) {
